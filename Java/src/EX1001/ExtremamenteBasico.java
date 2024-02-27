0b9ec6a1de27fca58e45b40155a0fd87d73f74c1
Vinicius Goulart Alves <vini.goulartalves@gmail.com>
2024-02-27 08:20:42 -0300

    commit - novo branch develop

--- Java/src/EX1001/ExtremamenteBasico.java
+++ Java/src/EX1001/ExtremamenteBasico.java
@@ -1,6 +1,8 @@
+package EX1001;
+
 import java.util.Scanner;
 
-public class EX1001 {
+public class ExtremamenteBasico {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
